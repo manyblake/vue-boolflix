43a210ef57bafd01a73b0b9ef459cb536bcdbc05
manyblake <manyblake@gmail.com>
2022-09-09 17:14:57 +0200

    import fontawesome

--- src/App.vue
+++ src/App.vue
@@ -3,25 +3,28 @@
     <input type="text" v-model="query" />
     <input type="submit" value="Cerca" @click="fetch()" />
     <ul>
-      <li v-for="(film, i) in films" :key="`f` + i">
+      <li v-for="film in films" :key="film.id">
         {{ film.original_title }}
         {{ film.title }}
-        {{ film.original_language }}
+        <img :src="`https://image.tmdb.org/t/p/w342/${film.poster_path}`" />
         <template v-if="flags.includes(film.original_language)">
-          <img :src="require(`./assets/img/${film.original_language}.png`)" />
+          <img :src="require(`./assets/img/${film.original_language}.svg`)" />
         </template>
         <template v-else>
           <img :src="genericFlag" />
         </template>
         {{ film.vote_average }}
+        <font-awesome-icon icon="fa-solid fa-star-half-stroke" />
+        <font-awesome-icon icon="fa-solid fa-star" />
+        <font-awesome-icon icon="fa-regular fa-star" />
       </li>
 
-      <li v-for="(serie, i) in series" :key="`s` + i">
+      <li v-for="serie in series" :key="serie.id">
         {{ serie.original_name }}
         {{ serie.name }}
         {{ serie.original_language }}
         <template v-if="flags.includes(serie.original_language)">
-          <img :src="require(`./assets/img/${serie.original_language}.png`)" />
+          <img :src="require(`./assets/img/${serie.original_language}.svg`)" />
         </template>
         <template v-else>
           <img :src="genericFlag" />
@@ -42,21 +45,22 @@ export default {
 
   data() {
     return {
-      api_key: `bc703ed01f56d8c42a9afde987ee066f`,
-      URI: `https://api.themoviedb.org/3/search`,
       query: ``,
       films: [],
       series: [],
-      flags: [`de`, `en`, `es`, `fr`, `it`, `uk`],
+      flags: [`de`, `en`, `es`, `fr`, `it`, `ja`, `pt`, `zh`],
       genericFlag: genericFlag,
     };
   },
 
   methods: {
     fetch() {
+      const api_key = `bc703ed01f56d8c42a9afde987ee066f`;
+      const URI = `https://api.themoviedb.org/3/search`;
+
       axios
         .get(
-          `${this.URI}/movie?api_key=${this.api_key}&language=it_IT&query=${this.query}`
+          `${URI}/movie?api_key=${api_key}&language=it_IT&query=${this.query}`
         )
         .then((response) => {
           this.films.push(...response.data.results);
--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import { library } from "@fortawesome/fontawesome-svg-core";
 
-Vue.config.productionTip = false
+/* import font awesome icon component */
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+/* import specific icons */
+import { faStarHalfStroke } from "@fortawesome/free-solid-svg-icons";
+import { faStar as fullStar } from "@fortawesome/free-solid-svg-icons";
+import { faStar as emptyStar } from "@fortawesome/free-regular-svg-icons";
+
+/* add icons to the library */
+library.add(faStarHalfStroke, fullStar, emptyStar);
+
+/* add font awesome icon component */
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
